4c5b5b60fb6b5d6a853fd05c2aae6d28bee016bc
fc191918 <hadji.nadir@courrier.uqam.ca>
2022-03-10 00:01:40 -0500

    Definition de la logique interne du module Solver

--- solver.h
+++ solver.h
@@ -85,3 +85,53 @@ bool above_check(int row, int column, char *position, char *word);
  * @return false if not.
  */
 bool under_check(int row, int column, char *position, char *word);
+
+/**
+ * @brief Verify if the word matches up, down, right or left of
+ * the poited char.
+ * 
+ * @param row row number of the char to check
+ * @param column column number if the char to check
+ * @param position Pointer to a char in the grid
+ * @param word Pointer to first char of a word
+ * @return Position structure {row,column,orientation} if found
+ * NULL if not.
+ */
+Position check_word(int row, int column, char *position, char *word);
+
+/**
+ * @brief Add a Position object to the list in arguments
+ * 
+ * @param position the Position object to add
+ * @param positions the Position array
+ */
+void add_found_position(Position position, Position positions[]);
+
+/**
+ * @brief Find the position of every word in the grid.
+ * 
+ * @param words array of words in grid
+ * @param grid matrice of caracteres where to search words
+ * @return Position* array of the positions found.
+ */
+Position* find_positions(char ** words, char** grid);
+
+/**
+ * @brief Replace all words in found on the grid previously
+ * by 0. At the end only the chars of the mystery word will
+ * remain in the grid. 
+ * 
+ * @param grid the grid to clean
+ * @param positions positions of the found words previously 
+ */
+void clean_grid(char **grid, Position positions[]);
+
+/**
+ * @brief Orchester the internal behaviour of the Solver Module.
+ * 
+ * @param grid Matrice of char 
+ * @param words array of words
+ * @return char* Mystery word in the grid.
+ */
+char* solve(char **grid, char **words);
+
